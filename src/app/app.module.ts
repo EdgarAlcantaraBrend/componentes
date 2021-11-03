37fd3a03785833a1202d86d1222388a75defe692
Edgar Alcantara <edgar.ginera1999@gmail.com>
2021-11-02 21:47:22 -0600

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,20 @@
+import { CssSelector } from '@angular/compiler';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ContadorComponent } from './contador/contador.component';
+import { CssComponent } from './css/css.component';
+import { JsComponent } from './js/js.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContadorComponent,
+    CssComponent,
+    JsComponent
   ],
   imports: [
     BrowserModule
